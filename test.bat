f4555ed1205f74a440c89f7c002472f533c6e0c6
Ahmed Wael <eng.ahmedwael205@gmail.com>
2020-04-21 10:40:40 +0200

    In The Terminal Type: "test.bat" to run the tests

---
+++ test.bat
@@ -0,0 +1,14 @@
+@echo [off]
+title GoTestCase
+python Tests.py GoTestCase1
+pause
+python Tests.py GoTestCase2
+pause
+python Tests.py GoTestCase3
+pause
+python Tests.py GoTestCase4
+pause
+python Tests.py GoTestCase5
+pause
+python Tests.py GoTestCase6
+pause
